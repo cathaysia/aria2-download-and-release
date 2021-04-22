b0620b79ad0c8e70da29d0eb0258766010bb401a
张龙涛 <319513897@outlook.com>
2021-04-22 22:32:59 +0800

    重新让 aria2 做种，尝试改善rar性能。下载哪吒

--- deal_download_files.py
+++ deal_download_files.py
@@ -5,15 +5,14 @@ os.system('rm -rf result')
 os.mkdir('result')
 
 for dirpath, dirnames, filenames in os.walk("downloads"):
+    os.chdir(dirpath)
     for filename in filenames:
         # 压缩文件
-        os.chdir(dirpath)
-        cmd = 'rar a -v1.5g "{rarfile}" "{sourcefile}"'.format(
+        cmd = 'rar a -v1.5g -sm5 -df "{rarfile}" "{sourcefile}"'.format(
             rarfile=path.join('../result', filename + '.rar'),
             sourcefile=filename
         )
         print("[deal] %s with %s" % (path.join(dirpath, filename), cmd))
         os.system(cmd)
         # 删除原始文件
-        os.unlink(filename)
-        os.chdir('..')
+    os.chdir('..')
